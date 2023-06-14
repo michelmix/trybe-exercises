7078bd48e8c08bc8d44d0025019d75e6c70e6e25
Michel Arantes <michelbarbosa@outlook.com>
2023-06-14 15:44:52 -0300

    Realiza exercicio2

---
+++ cs/secao-1-introducao-a-python/dia-1-aprendendo-python/exercicio2.py
@@ -0,0 +1,15 @@
+def average(list_numbers):
+    total = 0
+    for number in list_numbers:
+        total += number
+    return total / len(list_numbers)
+    
+
+print(average([2, 4, 6, 5, 9]))
+
+# def average(list_numbers):
+#     total = sum(list_numbers)
+#     return total / len(list_numbers)
+
+
+print(average([2, 4, 6, 5, 9]))
