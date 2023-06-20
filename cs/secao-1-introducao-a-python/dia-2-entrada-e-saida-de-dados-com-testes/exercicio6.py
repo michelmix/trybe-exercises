1122b5be2171300eac79183cd9cef21f77277476
Michel Arantes <michelbarbosa@outlook.com>
2023-06-20 17:42:22 -0300

    Realiza exercicio 6

---
+++ cs/secao-1-introducao-a-python/dia-2-entrada-e-saida-de-dados-com-testes/exercicio6.py
@@ -0,0 +1,44 @@
+def email_validation(email):
+    index = 0
+
+    # validar primeira letra
+    if not email[index].isalpha():
+        raise ValueError('Username should start with a letter')
+
+    # validar username
+    while email[index] != "@" and index < len(email):
+        letter = email[index]
+        if (
+            not letter.isalpha()
+            and not letter.isdigit()
+            and letter not in ('-', '_')
+        ):
+            raise ValueError(
+                "Username should contain only letters, "
+                "digits, dashes or underscores")
+        index += 1
+    index += 1
+
+    # validate extension
+    while email[index] != '.' and index < len(email):
+        letter = email[index]
+        if (
+            not letter.isalpha()
+            and not letter.isdigit()
+        ):
+            raise ValueError(
+                "Website should contain only letters, and digits"
+            )
+        index += 1
+    index += 1
+
+    counter = 0
+    while index < len(email):
+        counter += 1
+        index += 1
+    if counter > 3:
+        raise ValueError("Extension maximum length is 3")
+    return None
+
+
+email_validation('a-1@nomewebsite.esxt')
