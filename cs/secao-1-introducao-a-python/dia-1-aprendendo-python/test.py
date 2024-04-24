9eb639eaabfb873a1a589733c8ad32e31326136a
Michel Barbosa <michelbarbosa@outlook.com>
2024-04-24 10:53:14 -0300

    Salvando minhas alterações locais

---
+++ cs/secao-1-introducao-a-python/dia-1-aprendendo-python/test.py
@@ -0,0 +1,6 @@
+def is_odd(number):
+    'Retorna True se um número é ímpar, senão False.'
+    return number % 2 != 1
+
+
+# print(is_odd(4))
---
+++ cs/secao-3-Estruturas-de-dadosI-Listas-Lineares/dia-4-Pilhas-e-filas/exercicio1.py
@@ -0,0 +1,18 @@
+from dia-3-No-e-listas-encadeadas import LinkedList
+
+class Queue:
+
+    def __init__(self):
+        self.__data = LinkedList()
+
+    def is_empty(self):
+        return not len(self.__data)
+
+    def enqueue(self, value):
+        self.__data.insert_last(value)
+
+    def dequeue(self):
+        return self.__data.remove_first()
+
+    def peek(self):
+        return self.__data.get_element_at(0)
