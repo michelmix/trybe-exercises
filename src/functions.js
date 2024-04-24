fadd7dee5be43dba28c474940f2e7f210f5be746
Michel Barbosa <michelbarbosa@outlook.com>
2024-04-24 17:39:59 -0300

    Envia para repositorio pessoal

--- src/functions.js
+++ src/functions.js
@@ -3,18 +3,48 @@ function verifyPalindrome(word) {
   const reverseWord = word.split('').reverse('').join('');
   return word === reverseWord;
 }
-
-console.log(verifyPalindrome('arara'));
-console.log(verifyPalindrome('desenvolvimento'));
-
+// console.log(verifyPalindrome('arara'));
+// console.log(verifyPalindrome('desenvolvimentos'));
 // Requisito 2 - Crie a função getHighestIndex
-
+function getHighestIndex(array) {
+  const highestNumber = Math.max(...array);
+  return array.indexOf(highestNumber);
+}
+// let numeros = [2, 3, 4, 5, 6];
+// console.log(getHighestIndex(numeros));
 // Requisito 3 - Crie a função getSmallestIndex
+function getSmallestIndex(array) {
+  const highestNumber = Math.min(...array);
+  return array.indexOf(highestNumber);
+}
+// console.log(getSmallestIndex(numeros))
 
 // Requisito 4 - Crie a função getLongestWord
+function getLongestWord(array) {
+  let maior = '';
+  for (let palavra of array) {
+    if (palavra.length > maior.length) {
+      maior = palavra;
+    }
+  }
+  return maior;
+}
 
 // Requisito 5 - Crie a função countHighestNumberMaxOccurrences
+function countHighestNumberMaxOccurrences(array) {
+  let highestNumber = Math.max(...array);
+  let count = 0;
 
+  for (let number of array) {
+    if (number === highestNumber) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+array = [9, 1, 2, 3, 9, 5, 7, 8];
+console.log(countHighestNumberMaxOccurrences(array));
 // Não modifique as linhas abaixo
 module.exports = {
   verifyPalindrome:
